16c8acabe992c4107c32d53447db6cbfb220562b
Yi-Yang Chen <yachen@silabs.com>
2016-07-28 13:14:56 -0500

    Getting raw data as buffer objects

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const firebase = require('firebase')
 const path = require('path')
+const SerialPort = require('serialport')
 
 const app = firebase.initializeApp({
   // TODO: Setup service account for this app.
@@ -8,3 +9,25 @@ const app = firebase.initializeApp({
 })
 
 const db = app.database()
+
+// list and store serial ports
+SerialPort.list((err, ports) => {
+  var myPorts = []
+  ports.forEach((port) => {
+    console.log('Port Name: ' + port.comName)
+    myPorts.push(port)
+  })
+
+  var myPort = new SerialPort(myPorts[0].comName, { // Only supports Windows for now.
+    // parser: SerialPort.parsers.byteLength(1),
+    baudRate: 19200,
+  })
+
+  myPort.on('open', () => {
+    console.log('port open. Baudrate: ' + myPort.options.baudRate)
+  })
+
+  myPort.on('data', (data) => {
+    console.log(data)
+  })
+})
