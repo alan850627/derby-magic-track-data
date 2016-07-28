e92390eacb4ed4b623f7d926b1a56f699f4af4e2
Yi-Yang Chen <yachen@silabs.com>
2016-07-28 13:48:02 -0500

    buffer test

--- src/index.js
+++ src/index.js
@@ -11,23 +11,24 @@ const app = firebase.initializeApp({
 const db = app.database()
 
 // list and store serial ports
-SerialPort.list((err, ports) => {
-  var myPorts = []
-  ports.forEach((port) => {
-    console.log('Port Name: ' + port.comName)
-    myPorts.push(port)
-  })
-
-  var myPort = new SerialPort(myPorts[0].comName, { // Only supports Windows for now.
-    // parser: SerialPort.parsers.byteLength(1),
-    baudRate: 19200,
-  })
-
-  myPort.on('open', () => {
-    console.log('port open. Baudrate: ' + myPort.options.baudRate)
-  })
-
-  myPort.on('data', (data) => {
-    console.log(data)
-  })
-})
+// SerialPort.list((err, ports) => {
+//   var myPorts = []
+//   ports.forEach((port) => {
+//     console.log('Port Name: ' + port.comName)
+//     myPorts.push(port)
+//   })
+//
+//   var myPort = new SerialPort(myPorts[0].comName, { // Only supports Windows for now.
+//     // parser: SerialPort.parsers.byteLength(1),
+//     baudRate: 19200,
+//   })
+//
+//   myPort.on('open', () => {
+//     console.log('port open. Baudrate: ' + myPort.options.baudRate)
+//   })
+//
+//   myPort.on('data', (data) => {
+//     console.log(data)
+//   })
+// })
+//
