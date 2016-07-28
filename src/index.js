d317bcacac59fbd312dd1eb083f12900ff6adc84
Yi-Yang Chen <yachen@silabs.com>
2016-07-28 17:14:15 -0500

    remove unused

--- src/index.js
+++ src/index.js
@@ -10,12 +10,10 @@ const app = firebase.initializeApp({
 
 const db = app.database()
 const NUMBER_OF_LANES = 4
-const MAX_TIME = 9.9999
 
 // This object is what is eventually sent to firebase. SerialPort will populate this object as it gets data.
 var result = {}
 var ref = null
-var timeout = null
 var trackDataDB = db.ref().child('track-data')
 
 /**
@@ -29,8 +27,14 @@ parseStrData = function(str, obj = {}) {
   if (!str.includes('=')) {
     return obj
   }
-  let split = str.split('=')
-  obj[split[0]] = parseFloat(split[1].substring(0, 6))
+
+  str.split(' ').forEach((data) => { // If multiple datapoints come in at once, it's separated by space.
+    if (data.includes('=')) {
+      let split = data.split('=')
+      obj[split[0]] = parseFloat(split[1].substring(0, 6))
+    }
+  })
+
   return obj
 }
 
@@ -43,27 +47,13 @@ push = function(obj) {
 }
 
 /**
- * Some runs won't finish, so filling those with MAX_TIME
- * @param  obj [the result object to fill]
- * @return {[type]}     [description]
- */
-fillFailedRuns = function(obj) {
-  for (let i = 1; i <= NUMBER_OF_LANES; i++) {
-    if (!obj[i]) {
-      obj[i] = MAX_TIME
-    }
-  }
-  push(obj)
-}
-
-/**
  * Pushes the results to firebase.
  * If it is ready, push to firebase.
  * @param  obj [object to be checked and to be pushed to firebase]
  * @return {[type]}     [description]
  */
 pushResult = function(obj) {
-  if (Object.keys(obj).length === 1) {
+  if (!ref) {
     // First car has finished the race!
     // Wait for 10 seconds if some cars don't arrive.
     // Push a timestamp
@@ -73,14 +63,12 @@ pushResult = function(obj) {
       timestamp: Date.now(),
     }
     ref.update(time)
-    timeout = setTimeout(fillFailedRuns, 10000)
   }
 
   push(obj)
 
   if (Object.keys(obj).length === NUMBER_OF_LANES) {
     // all the cars finished
-    clearTimeout(timeout)
     ref = null
     result = {}
   }
